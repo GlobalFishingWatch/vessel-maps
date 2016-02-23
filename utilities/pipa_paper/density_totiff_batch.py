cbe51e11fcd9992303fa5e60ff088d6b7d574901
Kevin Wurster <kevin@skytruth.org>
2016-02-23 12:16:45 -0500

    + .idea

--- utilities/pipa_paper/density_totiff_batch.py
+++ utilities/pipa_paper/density_totiff_batch.py
@@ -44,7 +44,7 @@ def create_tifs(extra, cellsize):
 
     for f in onlyfiles:
         if ".npy" in f:
-            grid = np.load(source_dir + extra+ f)
+            grid = np.load(source_dir + extra+ f).astype(profile['dtype'])
             out_tif = out_dir + extra + f.replace('.npy',".tif")
             with rio.open(out_tif, 'w', **profile) as dst:
                 dst.write(grid, indexes=1)
